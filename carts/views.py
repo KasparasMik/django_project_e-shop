85c6a4b732017454c4c8c96995e3e0130fdbf5ef
Kasparas Mikenas <kasparascyber@gmail.com>
2022-06-21 13:40:37 +0200

    added product variation in cart item

--- carts/views.py
+++ carts/views.py
@@ -58,6 +58,12 @@ def add_cart(request, product_id):
     
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            for item in product_variation:
+                cart_item.variations.add(item)
+            
+            
         cart_item.quantity += 1 # cart item.qiantity = cart item.qiantity + 1
         cart_item.save()
     except CartItem.DoesNotExist:  # if not exist cart item, creating a new
@@ -66,6 +72,11 @@ def add_cart(request, product_id):
             quantity = 1,
             cart = cart,
         )
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            for item in product_variation:
+                cart_item.variations.add(item)
+            
         cart_item.save()
     return redirect('cart')
 
